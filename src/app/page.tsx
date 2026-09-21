e430d68b7845f74ef50d776f26f8b05033693178
clubnotcult <143037720+clubnotcult@users.noreply.github.com>
2026-09-21 15:56:59 -0500

    l

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function RootPage() {
-  // The Active Roll is the primary experience; send people straight there.
-  redirect("/active-roll");
-}
